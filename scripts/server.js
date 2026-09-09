c4d3621bfc2305dd666444218d8652c4bc36ab2f
Mostafa Elshorbagy <mostafaelshorbagyofficial@gmail.com>
2026-09-10 01:59:22 +0300

    Fix Vercel static deployment

--- scripts/server.js
+++ scripts/server.js
@@ -1,9 +1,9 @@
-﻿const http = require('http');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const PUBLIC_DIR = __dirname;
+const PUBLIC_DIR = path.join(__dirname, '..');
 
 const MIME_TYPES = {
     '.html': 'text/html; charset=utf-8',
